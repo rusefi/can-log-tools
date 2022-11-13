445acda316e7c0c2dcde00044f023ba2d0f93ff1
rusefillc <rusefillc@gmail.com>
2022-11-12 23:05:32 -0500

    CounterScanner v1

--- src/test/java/com/rusefi/can/analysis/CounterScannerTest.java
+++ src/test/java/com/rusefi/can/analysis/CounterScannerTest.java
@@ -3,12 +3,12 @@ package com.rusefi.can.analysis;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CounterScannerTest {
     @Test
     public void testNotCounter() {
         CounterScanner.BitState state = new CounterScanner.BitState();
-
         state.handle(true);
         state.handle(false);
         state.handle(false);
@@ -17,6 +17,16 @@ public class CounterScannerTest {
         state.handle(false);
 
         assertFalse(state.couldBeCounter);
+    }
 
+    @Test
+    public void testCounterLen1() {
+        CounterScanner.BitState state = new CounterScanner.BitState();
+        state.handle(true);
+        state.handle(false);
+        state.handle(true);
+        state.handle(false);
+
+        assertTrue(state.couldBeCounter);
     }
 }
