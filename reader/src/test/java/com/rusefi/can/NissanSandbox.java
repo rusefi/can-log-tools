dbdb57f19173a8d11e58e7d6dde5b9a9ed7598dd
rusefillc <sdfsdfqsf2334234234>
2024-02-24 17:45:18 -0500

    nissan

--- reader/src/test/java/com/rusefi/can/NissanSandbox.java
+++ reader/src/test/java/com/rusefi/can/NissanSandbox.java
@@ -6,6 +6,10 @@ public class NissanSandbox {
     public static void main(String[] args) throws IOException {
         String inputFolderName = "C:\\stuff\\rusefi_documentation\\OEM-Docs\\Nissan\\2011_Xterra\\2011-nissan-CAN-June-2021";
 
-        Launcher.main(new String[]{inputFolderName});
+        Launcher.main(new String[]{
+                inputFolderName,
+                Launcher.DBC_FILENAME_PROPERTY,
+                "opendbc/nissan_xterra_2011.dbc",
+        });
     }
 }
