d3246e3c6c3442ed040b6188af3438a8c5b9bc9c
Alexey Esaulenko <esaulenka@gmail.com>
2025-08-08 11:14:07 +0300

    Old java strings

--- reader/src/test/java/com/rusefi/can/reader/impl/J1939modeTest.java
+++ reader/src/test/java/com/rusefi/can/reader/impl/J1939modeTest.java
@@ -21,30 +21,30 @@ import static org.junit.Assert.*;
 public class J1939modeTest {
 
     // xxF004xx
-    private static final String dbcEEC = """
-    BO_ 2364540158 EEC1: 8 Vector__XXX
-        SG_ EngDemandPercentTorque : 56|8@1+ (1,-125) [-125|125] "%" Vector__XXX
-        SG_ EngStarterMode : 48|4@1+ (1,0) [0|15] "" Vector__XXX
-        SG_ SrcAddrssOfCntrllngDvcForEngCtrl : 40|8@1+ (1,0) [0|253] "" Vector__XXX
-        SG_ EngSpeed : 24|16@1+ (0.125,0) [0|8031.875] "rpm" Vector__XXX
-        SG_ ActualEngPercentTorque : 16|8@1+ (1,-125) [0|125] "%" Vector__XXX
-        SG_ DriversDemandEngPercentTorque : 8|8@1+ (1,-125) [0|125] "%" Vector__XXX
-        SG_ ActlEngPrcntTorqueHighResolution : 4|4@1+ (0.125,0) [0|0.875] "%" Vector__XXX
-        SG_ EngTorqueMode : 0|4@1+ (1,0) [0|15] "" Vector__XXX""";
+    private static final String dbcEEC =
+		"BO_ 2364540158 EEC1: 8 Vector__XXX\n" +
+		"    SG_ EngDemandPercentTorque : 56|8@1+ (1,-125) [-125|125] \"%\" Vector__XXX\n" +
+		"    SG_ EngStarterMode : 48|4@1+ (1,0) [0|15] \"\" Vector__XXX\n" +
+		"    SG_ SrcAddrssOfCntrllngDvcForEngCtrl : 40|8@1+ (1,0) [0|253] \"\" Vector__XXX\n" +
+		"    SG_ EngSpeed : 24|16@1+ (0.125,0) [0|8031.875] \"rpm\" Vector__XXX\n" +
+		"    SG_ ActualEngPercentTorque : 16|8@1+ (1,-125) [0|125] \"%\" Vector__XXX\n" +
+		"    SG_ DriversDemandEngPercentTorque : 8|8@1+ (1,-125) [0|125] \"%\" Vector__XXX\n" +
+		"    SG_ ActlEngPrcntTorqueHighResolution : 4|4@1+ (0.125,0) [0|0.875] \"%\" Vector__XXX\n" +
+		"    SG_ EngTorqueMode : 0|4@1+ (1,0) [0|15] \"\" Vector__XXX\n";
 
     // xxF013xx
-    private static final String dbcSSI = """
-    BO_ 2364543998 SSI: 8 Vector__XXX
-        SG_ RollAndPitchMeasurementLatency : 56|8@1+ (0.5,0) [0|125] "ms" Vector__XXX
-        SG_ PitchAndRollCompensated : 54|2@1+ (1,0) [0|3] "" Vector__XXX
-        SG_ PitchRateFigureOfMerit : 52|2@1+ (1,0) [0|3] "" Vector__XXX
-        SG_ RollAngleFigureOfMerit : 50|2@1+ (1,0) [0|3] "" Vector__XXX
-        SG_ PitchAngleFigureOfMerit : 48|2@1+ (1,0) [0|3] "" Vector__XXX
-        SG_ PitchRate : 32|16@1+ (0.002,-64) [-64|64.51] "deg/s" Vector__XXX
-        SG_ RollAngle : 16|16@1+ (0.002,-64) [-64|64.51] "deg" Vector__XXX
-        SG_ PitchAngle : 0|16@1+ (0.002,-64) [-64|64.51] "deg" Vector__XXX""";
+    private static final String dbcSSI =
+		"BO_ 2364543998 SSI: 8 Vector__XXX\n" +
+		"    SG_ RollAndPitchMeasurementLatency : 56|8@1+ (0.5,0) [0|125] \"ms\" Vector__XXX\n" +
+		"    SG_ PitchAndRollCompensated : 54|2@1+ (1,0) [0|3] \"\" Vector__XXX\n" +
+		"    SG_ PitchRateFigureOfMerit : 52|2@1+ (1,0) [0|3] \"\" Vector__XXX\n" +
+		"    SG_ RollAngleFigureOfMerit : 50|2@1+ (1,0) [0|3] \"\" Vector__XXX\n" +
+		"    SG_ PitchAngleFigureOfMerit : 48|2@1+ (1,0) [0|3] \"\" Vector__XXX\n" +
+		"    SG_ PitchRate : 32|16@1+ (0.002,-64) [-64|64.51] \"deg/s\" Vector__XXX\n" +
+		"    SG_ RollAngle : 16|16@1+ (0.002,-64) [-64|64.51] \"deg\" Vector__XXX\n" +
+		"    SG_ PitchAngle : 0|16@1+ (0.002,-64) [-64|64.51] \"deg\" Vector__XXX\n";
 
-//    @Test
+    @Test
     public void readEEC() throws IOException {
 
         Launcher.j1939_mode = true;
@@ -72,11 +72,10 @@ public class J1939modeTest {
         DbcFile dbc = ParseDBCTest.readDbc(dbcEEC + "\n\n" + dbcSSI);
 
         CANLineReader logReader = new PcanTrcReader1_1();
-        List<CANPacket> packets = List.of(
-                logReader.readLine("   5)        10.0  Rx     0CF00400  8  FF FF FF E8 25 FF FF 7D"),
-                logReader.readLine("  18)        30.0  Rx     0CF00400  8  FF FF FF 8C 26 FF FF 7D"),
-                logReader.readLine("  32)        50.0  Rx     0CF00400  8  FF FF FF 40 26 FF FF 7D")
-            );
+        List<CANPacket> packets = new ArrayList<>();
+        packets.add(logReader.readLine("   5)        10.0  Rx     0CF00400  8  FF FF FF E8 25 FF FF 7D"));
+        packets.add(logReader.readLine("  18)        30.0  Rx     0CF00400  8  FF FF FF 8C 26 FF FF 7D"));
+        packets.add(logReader.readLine("  32)        50.0  Rx     0CF00400  8  FF FF FF 40 26 FF FF 7D"));
 
         // dumb copy-paste from LoggingStrategy.writeLogByDbc()
 
