9faa9a6617ea82b456c34eb3493dbc5c6a5e658c
rusefi <rusefillc@gmail.com>
2025-10-26 23:38:24 -0400

    HexBinary

---
+++ reader/src/main/java/com/rusefi/io/can/isotp/HexUtil.java
@@ -0,0 +1,6 @@
+package com.rusefi.io.can.isotp;
+
+public class HexUtil {
+    private static final String HEX_STRING = "0123456789ABCDEF";
+    public static final char[] HEX_CHAR_ARRAY = HexUtil.HEX_STRING.toCharArray();
+}
