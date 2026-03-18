3433fc83d4bcda62f60c35e986ce33c1b617764f
rusefillc <rusefillc@gmail.com>
2026-03-18 06:19:17 -0400

    wrap

--- reader/src/main/java/com/rusefi/can/reader/impl/PcanTrcReader1_1.java
+++ reader/src/main/java/com/rusefi/can/reader/impl/PcanTrcReader1_1.java
@@ -12,22 +12,26 @@ public class PcanTrcReader1_1 implements CANLineReader {
 
     @Override
     public CANPacket readLine(String line, String fileName, int lineIndex) {
-        line = line.trim();
-        if (line.startsWith(PcanTrcReader2_0.FILEVERSION) && !line.startsWith(PcanTrcReader2_0.FILEVERSION + "=1.1"))
-            throw new IllegalStateException("Unexpected fileversion " + line);
-        if (line.startsWith(";"))
-            return null;
-        String[] tokens = line.split("\\s+");
-        if (tokens.length < 2)
-            throw new IllegalStateException("Unexpected token count in " + fileName + "@" + lineIndex + ": [" + line + "]");
-        double timeStampMs = Double.parseDouble(tokens[1]);
+        try {
+            line = line.trim();
+            if (line.startsWith(PcanTrcReader2_0.FILEVERSION) && !line.startsWith(PcanTrcReader2_0.FILEVERSION + "=1.1"))
+                throw new IllegalStateException("Unexpected fileversion " + line);
+            if (line.startsWith(";"))
+                return null;
+            String[] tokens = line.split("\\s+");
+            if (tokens.length < 2)
+                throw new IllegalStateException("Unexpected token count in " + fileName + "@" + lineIndex + ": [" + line + "]");
+            double timeStampMs = Double.parseDouble(tokens[1]);
 
-        int sid = Integer.parseInt(tokens[3], 16);
-        int size = Integer.parseInt(tokens[4]);
+            int sid = Integer.parseInt(tokens[3], 16);
+            int size = Integer.parseInt(tokens[4]);
 
-        byte[] data = CANLineReader.readHexArray(tokens, 5, size);
+            byte[] data = CANLineReader.readHexArray(tokens, 5, size);
 
 
-        return new CANPacket(timeStampMs, sid, data);
+            return new CANPacket(timeStampMs, sid, data);
+        } catch (Exception e) {
+            throw new IllegalStateException("While [" + line + "] of " + fileName + "@" + lineIndex, e);
+        }
     }
 }
