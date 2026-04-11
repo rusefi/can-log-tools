8d0b5cf1bb2bb149272304c74cc6627601cf790f
rusefillc <rusefillc@gmail.com>
2026-04-11 14:18:44 -0400

    test data & skeleton

--- reader/src/main/java/com/rusefi/can/mlv/CanToMegaLogViewerSandbox.java
+++ reader/src/main/java/com/rusefi/can/mlv/CanToMegaLogViewerSandbox.java
@@ -1,7 +1,21 @@
 package com.rusefi.can.mlv;
 
-public class CanToMegaLogViewerSandbox {
-    public static void main(String[] args) {
+import com.rusefi.can.CANPacket;
+import com.rusefi.can.Launcher;
+import com.rusefi.can.reader.impl.AutoFormatReader;
 
+import java.io.IOException;
+import java.util.List;
+
+public class CanToMegaLogViewerSandbox {
+    public static void main(String[] args) throws IOException {
+        String dbcFileName = "C:\\stuff\\can-log-tools\\reader\\src\\test\\resources\\test_motorola.dbc";
+        String trcFileName = "C:\\stuff\\can-log-tools\\reader\\src\\test\\resources\\test_motorola.trc";
+
+        Launcher.dbcFileName = dbcFileName;
+
+        List<CANPacket> packets = AutoFormatReader.INSTANCE.readFile(trcFileName);
+
+        CanToMegaLogViewer.createMegaLogViewer(".", packets, "test_motorola");
     }
 }
