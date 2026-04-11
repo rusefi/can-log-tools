857bd701b6dba197c516eaeb7eb84e8b0e0cde90
rusefillc <rusefillc@gmail.com>
2026-04-10 23:45:46 -0400

    docs

--- model/src/main/java/com/rusefi/can/DualSid.java
+++ model/src/main/java/com/rusefi/can/DualSid.java
@@ -3,6 +3,7 @@ package com.rusefi.can;
 public class DualSid {
     public static final String HEX_ID_FORMAT = "%03X";
 
+    // convention: decimal first, upper case three digit hex second
     private static String dualSid(int sid, String separator) {
         return String.format("%d%s" + HEX_ID_FORMAT, sid, separator, sid);
     }
