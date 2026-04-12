33dcba103187dfc75beaa98f4a117652fc884f01
rusefi <rusefillc@gmail.com>
2026-04-11 20:26:31 -0400

    PacketFrequency

--- reader/src/main/java/com/rusefi/can/analysis/PacketFrequency.java
+++ reader/src/main/java/com/rusefi/can/analysis/PacketFrequency.java
@@ -19,7 +19,7 @@ import java.util.*;
 public class PacketFrequency {
     private static final String OUTPUT_FILE_PREFIX = "frequency_";
 
-    private static final int[] WELL_KNOWN_FREQUENCIES = {10, 20, 50, 100, 200, 500, 1000};
+    private static final int[] WELL_KNOWN_FREQUENCIES = {10, 20, 50, 100, 200, 500, 1000, 2000};
     private static final double TOLERANCE = 0.05;
 
     public static void main(String[] args) throws IOException {
